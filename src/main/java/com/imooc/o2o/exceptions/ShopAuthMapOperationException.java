2248007c230fa068a381ff2ba6501af68f75a932
Pat Chen <pchen6851@gmail.com>
2020-09-01 23:12:10 -0500

    service layer of ShopAuthMap

---
+++ src/main/java/com/imooc/o2o/exceptions/ShopAuthMapOperationException.java
@@ -0,0 +1,14 @@
+package com.imooc.o2o.exceptions;
+
+public class ShopAuthMapOperationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7440278813025975274L;
+
+	public ShopAuthMapOperationException(String msg) {
+		super(msg);
+	}
+
+}
